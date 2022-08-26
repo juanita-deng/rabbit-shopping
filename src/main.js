6710f3312a067ffcc45feb1eb65fce192d4e06fc
juanita <1182926231@qq.com>
2022-08-26 20:22:22 +0800

    chore:引入normalize

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 导入样式处理文件,类似reset.css,相对更加平和
+import 'normalize.css'
 createApp(App).use(store).use(router).mount('#app')
