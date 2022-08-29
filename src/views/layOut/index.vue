3dfa09ffc551095f9f2d61eafd814639bb51b9e1
juanita <1182926231@qq.com>
2022-08-29 16:48:24 +0800

    feat:footer组件

--- src/views/layOut/index.vue
+++ src/views/layOut/index.vue
@@ -1,12 +1,22 @@
 <template>
   <TopBar></TopBar>
   <Header></Header>
+  <main class="app-body">
+    <router-view></router-view>
+  </main>
+  <Footer></Footer>
 </template>
 <script>
 import TopBar from './components/topbar.vue'
 import Header from './components/header.vue'
+import Footer from './components/footer.vue'
 export default {
   name: 'layout',
-  components: { TopBar, Header }
+  components: { TopBar, Header, Footer }
 }
 </script>
+<style lang="less" scoped>
+.app-body {
+  min-height: 600px;
+}
+</style>
