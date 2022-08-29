5324346c2271ed3305b44f2ee4ef949864d31951
juanita <1182926231@qq.com>
2022-08-29 17:18:31 +0800

    feat:导航栏一级分类的提取

---
+++ src/views/layOut/components/first-category.vue
@@ -0,0 +1,100 @@
+<template>
+  <ul class="navs">
+    <li class="home"><RouterLink to="/">首页</RouterLink></li>
+    <li>
+      <a href="#">美食</a>
+      <div class="layer">
+        <ul>
+          <li v-for="i in 10" :key="i">
+            <a href="#">
+              <img
+                src="https://yanxuan.nosdn.127.net/cc361cf40d4f81c7eccefed1ad18face.png?quality=95&imageView"
+                alt=""
+              />
+              <p>果干</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    <li><a href="#">餐厨</a></li>
+    <li><a href="#">艺术</a></li>
+    <li><a href="#">电器</a></li>
+    <li><a href="#">居家</a></li>
+    <li><a href="#">洗护</a></li>
+    <li><a href="#">孕婴</a></li>
+    <li><a href="#">服装</a></li>
+    <li><a href="#">杂货</a></li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'first-category'
+}
+</script>
+<style lang="less" scoped>
+.navs {
+  width: 820px;
+  display: flex;
+  justify-content: space-around;
+  padding-left: 40px;
+  position: relative;
+  z-index: 999;
+  > li {
+    margin-right: 40px;
+    width: 38px;
+    text-align: center;
+    > a {
+      font-size: 16px;
+      line-height: 32px;
+      height: 32px;
+      display: inline-block;
+    }
+    &:hover {
+      > a {
+        color: @xtxColor;
+        border-bottom: 1px solid @xtxColor;
+      }
+      > .layer {
+        height: 132px;
+        opacity: 1;
+      }
+    }
+  }
+  .layer {
+    width: 1240px;
+    background-color: #fff;
+    position: absolute;
+    left: -200px;
+    top: 56px;
+    height: 0;
+    overflow: hidden;
+    opacity: 0;
+    box-shadow: 0 0 5px #ccc;
+    transition: all 0.2s 0.1s;
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0 70px;
+      align-items: center;
+      height: 132px;
+      li {
+        width: 110px;
+        text-align: center;
+        img {
+          width: 60px;
+          height: 60px;
+        }
+        p {
+          padding-top: 10px;
+        }
+        &:hover {
+          p {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/layOut/components/header.vue
+++ src/views/layOut/components/header.vue
@@ -2,18 +2,7 @@
   <header class="app-header">
     <div class="container">
       <h1 class="logo"><RouterLink to="/">小兔鲜</RouterLink></h1>
-      <ul class="navs">
-        <li class="home"><RouterLink to="/">首页</RouterLink></li>
-        <li><a href="#">美食</a></li>
-        <li><a href="#">餐厨</a></li>
-        <li><a href="#">艺术</a></li>
-        <li><a href="#">电器</a></li>
-        <li><a href="#">居家</a></li>
-        <li><a href="#">洗护</a></li>
-        <li><a href="#">孕婴</a></li>
-        <li><a href="#">服装</a></li>
-        <li><a href="#">杂货</a></li>
-      </ul>
+      <FirstCategory></FirstCategory>
       <div class="search">
         <i class="iconfont icon-search"></i>
         <input type="text" placeholder="搜一搜" />
@@ -27,8 +16,10 @@
   </header>
 </template>
 <script>
+import FirstCategory from './first-category.vue'
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  components: { FirstCategory }
 }
 </script>
 <style scoped lang="less">
@@ -49,29 +40,7 @@ export default {
         contain;
     }
   }
-  .navs {
-    width: 820px;
-    display: flex;
-    justify-content: space-around;
-    padding-left: 40px;
-    > li {
-      margin-right: 40px;
-      width: 38px;
-      text-align: center;
-      > a {
-        font-size: 16px;
-        line-height: 32px;
-        height: 32px;
-        display: inline-block;
-      }
-      > &:hover {
-        > a {
-          color: @xtxColor;
-          border-bottom: 1px solid @xtxColor;
-        }
-      }
-    }
-  }
+
   .search {
     width: 170px;
     height: 32px;
