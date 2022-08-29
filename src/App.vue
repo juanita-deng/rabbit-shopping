b47de273ef0c22a62bb36c11b92e6958069899e4
juanita <1182926231@qq.com>
2022-08-29 14:48:21 +0800

    feat:添加路由文件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
   <router-view />
-  <div class="test"></div>
 </template>
 
 <script>
-import instance from '@/utils/request'
 export default {
-  setup() {
-    instance({
-      method: 'get',
-      url: '/home/index'
-    }).then((res) => {
-      console.log('res', res)
-    })
-  }
+  setup() {}
 }
 </script>
-<style lang="less" scoped>
-.test {
-  width: 100px;
-  height: 100px;
-  background-color: @xtxColor;
-  .hoverShadow ();
-}
-</style>
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routes = []
+import layOut from '@/views/layOut'
+const routes = [
+  { path: '/', component: layOut },
+  { path: '/login', component: () => import('@/views/login') } // 路由懒加载
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
