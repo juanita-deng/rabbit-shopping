1f2023320b849f2917a707586bc47e217c68aec2
juanita <1182926231@qq.com>
2022-08-31 16:08:58 +0800

    feat:注册全局骨架屏组件

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- 测试全局骨架屏组件 -->
+  <!-- <XtxSkeleton width="200px" height="100px" animated bg="red" /> -->
   <router-view />
 </template>
 
--- src/main.js
+++ src/main.js
@@ -7,4 +7,11 @@ import 'normalize.css'
 import '@/assets/styles/common.less'
 // 导入模拟mock数据(会优先走本地mock数据,接口调通后可删除)
 // import './mock'
-createApp(App).use(store).use(router).mount('#app')
+
+// import XtxSkeleton from '@/components/library/rabbit-skeleton.vue'
+createApp(App)
+  .use(store)
+  .use(router)
+  // 注册全局组件
+  //   .component(XtxSkeleton.name, XtxSkeleton)
+  .mount('#app')
