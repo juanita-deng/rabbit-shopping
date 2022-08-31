833289fdf1868ba05e7febb05eb8aa1793ced638
juanita <1182926231@qq.com>
2022-08-31 16:33:14 +0800

    feat:插件方式注册全局骨架屏组件

--- src/main.js
+++ src/main.js
@@ -8,10 +8,13 @@ import '@/assets/styles/common.less'
 // 导入模拟mock数据(会优先走本地mock数据,接口调通后可删除)
 // import './mock'
 
+import rabbitUI from '@/components/library/index'
 // import XtxSkeleton from '@/components/library/rabbit-skeleton.vue'
 createApp(App)
   .use(store)
   .use(router)
+  // 注册插件
+  .use(rabbitUI)
   // 注册全局组件
   //   .component(XtxSkeleton.name, XtxSkeleton)
   .mount('#app')
--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -8,13 +8,24 @@
         @mouseenter="currentCategoryId = i.id"
       >
         <RouterLink :to="`/category/${i.id}`">{{ i.name }}</RouterLink>
-        <RouterLink
-          v-for="item in i.children"
-          :key="item.id"
-          :to="`/category/sub/${i.id}`"
-        >
-          {{ item.name }}
-        </RouterLink>
+        <template v-if="i.children">
+          <RouterLink
+            v-for="item in i.children"
+            :key="item.id"
+            :to="`/category/sub/${i.id}`"
+          >
+            {{ item.name }}
+          </RouterLink>
+        </template>
+        <template v-else>
+          <XtxSkeleton
+            width="60px"
+            height="18px"
+            style="margin-right: 5px"
+            bg="rgba(255,255,255,0.2)"
+          />
+          <XtxSkeleton width="50px" height="18px" bg="rgba(255,255,255,0.2)" />
+        </template>
       </li>
     </ul>
     <!-- 商品分类弹层 -->
@@ -113,6 +124,17 @@ export default {
       padding-left: 40px;
       height: 50px;
       line-height: 50px;
+      .xtx-skeleton {
+        animation: fade 1s linear infinite alternate;
+      }
+      @keyframes fade {
+        from {
+          opacity: 0.2;
+        }
+        to {
+          opacity: 1;
+        }
+      }
       &:hover,
       &.active {
         background: @xtxColor;
