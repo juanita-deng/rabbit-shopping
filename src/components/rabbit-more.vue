227876e0f756ae6a7673976513c624d67168009d
juanita <1182926231@qq.com>
2022-09-15 21:43:16 +0800

    feat:查看更多组件封装

---
+++ src/components/rabbit-more.vue
@@ -0,0 +1,46 @@
+<template>
+  <RouterLink :to="path" class="xtx-more">
+    <span>
+      <slot>查看更多</slot>
+    </span>
+    <i class="iconfont icon-angle-right"></i>
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  name: 'RabbitMore',
+  props: {
+    path: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.xtx-more {
+  margin-bottom: 2px;
+  display: flex;
+  span {
+    font-size: 16px;
+    vertical-align: middle;
+    margin-right: 4px;
+    color: #999;
+  }
+  i {
+    font-size: 14px;
+    vertical-align: middle;
+    position: relative;
+    top: 2px;
+    color: #ccc;
+  }
+  &:hover {
+    span,
+    i {
+      color: @xtxColor;
+    }
+  }
+}
+</style>
