1e1138bd0df705a15f9dad3153b0a1adc5f897eb
juanita <1182926231@qq.com>
2022-09-15 22:14:27 +0800

    feat:自动化注册全局组件

--- src/components/index.js
+++ src/components/index.js
@@ -6,17 +6,35 @@
  * @author juanit-deng
  * @time 2022-08-31
  * */
-import RabbitSkeleton from '@/components/rabbit-skeleton.vue'
-import RabbitCarousel from '@/components/rabbit-carousel.vue'
-import RabbitMore from '@/components/rabbit-more.vue'
+// import RabbitSkeleton from '@/components/rabbit-skeleton.vue'
+// import RabbitCarousel from '@/components/rabbit-carousel.vue'
+// import RabbitMore from '@/components/rabbit-more.vue'
 export default {
   install(app) {
     /*
         在app上进行扩展，app提供 component directive 函数
         如果要挂载原型 app.config.globalProperties 方式
     */
-    app.component(RabbitSkeleton.name, RabbitSkeleton)
-    app.component(RabbitCarousel.name, RabbitCarousel)
-    app.component(RabbitMore.name, RabbitMore)
+    // app.component(RabbitSkeleton.name, RabbitSkeleton)
+    // app.component(RabbitCarousel.name, RabbitCarousel)
+    // app.component(RabbitMore.name, RabbitMore)
+
+    /**
+     * require.context()是webpack提供的一个自动化导入的API
+     * @parm1 加载的文件目录
+     * @pram2 是否加载子目录
+     * @pram3 正则,匹配文件
+     * @return 返回导入的函数fn
+     * @method .keys()获取读取到的所有文件列表
+     */
+    // 加载components下所有的vue文件
+    const ctx = require.context('./', false, /\.vue$/)
+    // console.log('ctx', ctx)
+    ctx.keys().forEach((path) => {
+      // path 组件的地址
+      const component = ctx(path).default
+      // console.log('component', component)
+      app.component(component.name, component)
+    })
   }
 }
