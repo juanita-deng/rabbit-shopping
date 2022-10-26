82562c74922255069157ce713f3c1e3a82660d87
juanita <1182926231@qq.com>
2022-10-26 15:38:29 +0800

    feat:封装全局消息组件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   <!-- <RabbitSkeleton width="200px" height="100px" animated bg="red" /> -->
   <!-- 测试自定义指令的使用 -->
   <!-- <input type="text" v-focus style="margin: 10px" /> -->
+  <!-- 测试全局组件message -->
+  <!-- <RabbitMessage type="error" text="校验失败"></RabbitMessage> -->
   <router-view />
 </template>
 
