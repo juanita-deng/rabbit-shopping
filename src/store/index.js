9809e4967756eb20a6aaa327a6c106d519170ce8
juanita <1182926231@qq.com>
2022-10-27 12:09:19 +0800

    feat:登录功能-vuex-persistedstate数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import category from './modules/category'
 import user from './modules/user'
 export default createStore({
@@ -6,5 +7,17 @@ export default createStore({
   modules: {
     category,
     user
-  }
+  },
+  // 用于配制插件
+  plugins: [
+    createPersistedState({
+      /**
+       * 官方文档:https://github.com/robinvdvleuten/vuex-persistedstate
+       * 默认是将整个store里所有模块都储存在localstorage中,且名字为vuex
+       */
+      key: 'rabbit-shopping', // 配制存储的名字
+      paths: ['user'], // 指定需要数据持久化的模块
+      storage: localStorage // 指定数据存储的位置,默认localStorage,还可配置sessionStorage,cookie(cookie需要额外配置)
+    })
+  ]
 })
