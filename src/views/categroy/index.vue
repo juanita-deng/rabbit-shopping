687f1febbaaeb9f0f41d529d9bc07043d771f670
juanita <1182926231@qq.com>
2022-11-03 17:20:22 +0800

    feat:顶级组件-面包屑渲染

--- src/views/categroy/index.vue
+++ src/views/categroy/index.vue
@@ -2,13 +2,26 @@
 <template>
   <RabbitBread separatorClass="icon-angle-right">
     <RabbitBreadItem to="/">首页</RabbitBreadItem>
-    <RabbitBreadItem to="/category/10000">电器</RabbitBreadItem>
-    <RabbitBreadItem>冰箱</RabbitBreadItem>
+    <RabbitBreadItem :to="`/category/${category?.id}`">
+      {{ category?.name }}
+    </RabbitBreadItem>
   </RabbitBread>
 </template>
 <script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+
 export default {
-  name: 'category'
+  name: 'category',
+  setup() {
+    const store = useStore()
+    const route = useRoute()
+    const category = computed(() => {
+      return store.state.category.list.find((v) => v.id === route.params.id)
+    })
+    return { category }
+  }
 }
 </script>
 <style lang="less" scoped></style>
