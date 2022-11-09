eb6caf9dca7529bf9e03ae1fd14bdadb4fcd63ac
juanita <1182926231@qq.com>
2022-11-09 17:58:33 +0800

    feat:商品详情-城市选择交互

--- src/components/rabbit-city.vue
+++ src/components/rabbit-city.vue
@@ -6,14 +6,22 @@
       <i class="iconfont icon-angle-down"></i>
     </div>
     <div class="option" v-if="active">
-      <span class="ellipsis" v-for="i in 24" :key="i">北京市</span>
+      <span
+        class="ellipsis"
+        v-for="i in cityList"
+        :key="i.code"
+        @click="changeName(i)"
+      >
+        {{ i.name }}
+      </span>
     </div>
   </div>
 </template>
 
 <script>
 import { onClickOutside } from '@vueuse/core'
-import { ref } from 'vue'
+import axios from 'axios'
+import { ref, reactive, watch } from 'vue'
 export default {
   name: 'RabbitCity',
   setup() {
@@ -22,8 +30,55 @@ export default {
     onClickOutside(target, () => {
       active.value = false
     })
-    return { active, target }
+    const { cityList, cacheList } = getCityList()
+    // 选择城市需要返回给父组件的值
+    const changeResult = reactive({
+      provinceCode: '',
+      provinceName: '',
+      cityCode: '',
+      cityName: '',
+      countyCode: '',
+      countyName: ''
+    })
+    const changeName = (i) => {
+      cityList.value = i.areaList
+      if (i.level === 0) {
+        // 收集省信息
+        changeResult.provinceCode = i.code
+        changeResult.provinceName = i.name
+      } else if (i.level === 1) {
+        // 收集市信息
+        changeResult.cityCode = i.code
+        changeResult.cityName = i.name
+      } else if (i.level === 2) {
+        // 收集县信息
+        changeResult.countyCode = i.code
+        changeResult.countyName = i.name
+        // 关闭弹窗
+        active.value = false
+      }
+      // 监听关闭弹窗的处理,关闭后恢复数据(即可解决:每次重新选择为最新的全量数据)
+      watch(active, (val) => {
+        if (!val) {
+          cityList.value = cacheList.value
+        }
+      })
+    }
+    return { active, target, cityList, changeName }
   }
+}
+// 注意:城市信息数据是从阿里云上获取的,所以不用封装的request
+const getCityList = () => {
+  const cityList = ref([]) // 用于渲染
+  const cacheList = ref([]) // 用于恢复数据
+  axios({
+    method: 'get',
+    url: 'https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/area.json'
+  }).then(({ data }) => {
+    cityList.value = data
+    cacheList.value = data
+  })
+  return { cityList, cacheList }
 }
 </script>
 
