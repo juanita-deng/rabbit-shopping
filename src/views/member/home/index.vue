743abf791284837ecb85072f738bb2057280ef9e
juanita <1182926231@qq.com>
2022-12-20 17:56:28 +0800

    feat:个人中心-渲染完成

--- src/views/member/home/index.vue
+++ src/views/member/home/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>个人中心</div>
+  <div class="member-home">
+    <HomeOverview />
+    <HomePanel title="收藏的商品"  />
+    <HomePanel title="我的足迹" />
+    <GoodsRecommend title="猜你喜欢" />
+  </div>
 </template>
-
 <script>
+import HomeOverview from './components/home-overview'
+import HomePanel from './components/home-panel'
+import GoodsRecommend from '@/views/goods/components/goods-recommend.vue'
 export default {
-
+  name: 'MemberHome',
+  components: {
+    HomeOverview,
+    HomePanel,
+    GoodsRecommend
+  }
 }
 </script>
-
-<style>
-
+<style scoped lang="less">
+:deep(.xtx-carousel) .carousel-btn.prev {
+  left: 5px;
+}
+:deep(.xtx-carousel) .carousel-btn.next {
+  right: 5px;
+}
 </style>
