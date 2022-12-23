c23871293cb8548a7e2b0dd2f2cb076821ef3722
juanita <1182926231@qq.com>
2022-12-23 09:41:00 +0800

    feat:我的订单-动态渲染

--- src/api/order.js
+++ src/api/order.js
@@ -12,7 +12,7 @@ export const findCheckoutInfo = () => {
  * 添加收货地址信息
  * @param {Object} address - 地址对象
  */
-export const addAddress = address => {
+export const addAddress = (address) => {
   return request({
     url: '/member/address',
     method: 'post',
@@ -23,7 +23,7 @@ export const addAddress = address => {
  * 编辑收货地址信息
  * @param {Object} address - 地址对象
  */
-export const editAddress = address => {
+export const editAddress = (address) => {
   return request({
     method: 'put',
     url: `/member/address/${address.id}`,
@@ -34,7 +34,7 @@ export const editAddress = address => {
  * 提交订单
  * @param {Object} order - 订单信息对象
  */
-export const createOrder = order => {
+export const createOrder = (order) => {
   return request({
     method: 'post',
     url: '/member/order',
@@ -45,9 +45,23 @@ export const createOrder = order => {
  * 获取订单详情
  * @param {String} id - 订单ID
  */
-export const findOrder = id => {
+export const findOrder = (id) => {
   return request({
     method: 'get',
     url: '/member/order/' + id
+  })
+}
+/**
+ * 查询订单列表
+ * @param {Number} orderState - 订单状态，1为待付款、2为待发货、3为待收货、4为待评价、5为已完成、6为已取消，未传该参数或0为全部
+ * @param {Number} page - 页码
+ * @param {Number} pageSize - 每页条数
+ * @returns
+ */
+export const findOrderList = ({ orderState = 0, page = 1, pageSize = 10 }) => {
+  return request({
+    url: '/member/order',
+    method: 'get',
+    params: { orderState, page, pageSize }
   })
 }
--- src/views/member/order/index.vue
+++ src/views/member/order/index.vue
@@ -1,31 +1,56 @@
 <template>
-  <div>
-    <RabbitTabs v-model="active" @tabClick="tabClick">
-      <RabbitTabsPanel
-        v-for="i in orderStatus"
-        :key="i.name"
-        :label="i.label"
-        :name="i.name"
-      >
-        内容7
-      </RabbitTabsPanel>
-    </RabbitTabs>
+  <RabbitTabs v-model="active" @tabClick="tabClick">
+    <RabbitTabsPanel
+      v-for="i in orderStatus"
+      :key="i.name"
+      :label="i.label"
+      :name="i.name"
+    />
+  </RabbitTabs>
+  <!-- 订单列表 -->
+  <div class="order-list">
+    <OrderPanel
+      v-for="order in orderList.items"
+      :key="order.id"
+      :order="order"
+    />
   </div>
+  <RabbitPagination />
 </template>
 
 <script>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { orderStatus } from '@/api/constant'
+import { findOrderList } from '@/api/order'
+import OrderPanel from './components/order-panel.vue'
 export default {
   name: 'RabbitOrder',
+  components: {
+    OrderPanel
+  },
   setup() {
     const active = ref('all')
     const tabClick = ({ name, label }) => {
       console.log('name', name, label)
     }
-    return { active, tabClick, orderStatus }
+    const reqParm = reactive({
+      page: 1,
+      pageSize: 10,
+      orderState: 0
+    })
+    const orderList = ref({})
+    findOrderList(reqParm).then(({ result }) => {
+      console.log('res', result)
+      orderList.value = result
+    })
+    return { active, tabClick, orderStatus, orderList }
   }
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.order-list {
+  background: #fff;
+  padding: 20px;
+}
+</style>
