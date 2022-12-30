d618744ff975bda5fcbf61850954fa8e154c9780
juanita <1182926231@qq.com>
2022-12-30 16:26:07 +0800

    feat:我的订单-订单详情

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>订单详情</div>
+  <div class="order-detail-page" v-if="order">
+    <!-- 操作栏 -->
+    <DetailAction :order="order" />
+    <!-- 步骤条 组件xtx-steps.vue-->
+    <!-- 物流栏 -->
+    <!-- 订单商品信息 -->
+  </div>
 </template>
-
 <script>
-export default {}
+import DetailAction from './components/detail-action'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import { findOrder } from '@/api/order'
+export default {
+  name: 'OrderDetailPage',
+  components: { DetailAction },
+  setup() {
+    const order = ref(null)
+    const route = useRoute()
+    findOrder(route.params.id).then((data) => {
+      order.value = data.result
+    })
+    return {
+      order
+    }
+  }
+}
 </script>
-
-<style></style>
+<style scoped lang="less">
+.order-detail-page {
+  background: #fff;
+}
+</style>
